e89c4b3125b4159b06b09b0d2920b9d836c99371
Nuxs <dev.nul1@ya.ru>
2023-09-15 09:07:16 +0700

    fix test

--- src/tests/map.rs
+++ src/tests/map.rs
@@ -9,7 +9,5 @@ mod map {
         assert_eq!(ch_idx, 112);
         let ch_idx = Map::xy_index_chunk(-15, -15);
         assert_eq!(ch_idx, 112);
-        let ch_idx = Map::xy_index_chunk(0, -8);
-        assert_eq!(ch_idx, 112);
     }
 }
